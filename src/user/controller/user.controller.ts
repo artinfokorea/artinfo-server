5f10c39b6d49bcfccf6605c60da6dbd00eb5a21d
daniel <daniel@e-connect.kr>
2024-06-30 16:58:39 +0900

    main: 비밀번호 변경 권한 제거

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -28,7 +28,7 @@ export class UserController {
     return new OkResponse();
   }
 
-  @RestApiPut(OkResponse, { path: '/me/password', description: '내 비밀번호 수정', auth: [USER_TYPE.CLIENT] })
+  @RestApiPut(OkResponse, { path: '/me/password', description: '내 비밀번호 수정' })
   async editUserPassword(@Signature() signature: UserSignature, @Body() request: EditUserPasswordRequest) {
     await this.userService.editPassword(signature.email, request.password);
 
