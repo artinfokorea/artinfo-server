5efb9e818020bd54a3c22b37b81e5dd1675406c8
Seongjun Im <80080990+choral7451@users.noreply.github.com>
2025-03-22 22:50:03 +0900

    perf: top posts 조회 필터 추가

--- src/post/repository/PostRepository.ts
+++ src/post/repository/PostRepository.ts
@@ -13,55 +13,63 @@ export class PostRepository extends Repository<PostEntity> {
     super(PostEntity, dataSource.createEntityManager());
   }
 
-  findTop() {
-    return this.createQueryBuilder('post')
+  async findTop() {
+    const startDate = new Date();
+    startDate.setDate(1);
+    startDate.setHours(0, 0, 0, 0);
+
+    const endDate = new Date(startDate);
+    endDate.setMonth(endDate.getMonth() + 1);
+    endDate.setDate(0);
+    endDate.setHours(23, 59, 59, 999);
+
+    const result = await this.createQueryBuilder('post')
       .leftJoinAndSelect('post.user', 'user')
       .leftJoin('post.likes', 'likes')
       .leftJoin('post.comments', 'comments', 'comments.type = :type', { type: COMMENT_TYPE.POST })
       .addSelect('COUNT(likes.id)', 'likesCount')
       .addSelect('COUNT(comments.id)', 'commentsCount')
+      .where('post.createdAt BETWEEN :startDate AND :endDate', { startDate, endDate })
       .groupBy('post.id')
       .addGroupBy('user.id')
       .orderBy('COUNT(likes.id) + COUNT(comments.id)', 'DESC')
       .addOrderBy('post.createdAt', 'DESC')
       .limit(10)
-      .getRawMany()
-      .then(result => {
-        return result.map(rawPost => {
-          const user = new User().fromRaw({
-            user_id: rawPost['user_id'],
-            user_type: rawPost['user_type'],
-            user_name: rawPost['user_name'],
-            user_nickname: rawPost['user_nickname'],
-            user_email: rawPost['user_email'],
-            user_phone: rawPost['user_phone'],
-            user_birth: rawPost['user_birth'],
-            user_password: rawPost['user_password'],
-            user_icon_image_url: rawPost['user_icon_image_url'],
-            user_created_at: rawPost['user_created_at'],
-            user_updated_at: rawPost['user_updated_at'],
-            user_deleted_at: rawPost['user_deleted_at'],
-          });
-
-          const postEntity: Partial<PostEntity> = {
-            id: rawPost['post_id'],
-            userId: rawPost['post_user_id'],
-            user: user,
-            category: rawPost['post_category'],
-            title: rawPost['post_title'],
-            contents: rawPost['post_contents'],
-            thumbnailImageUrl: rawPost['post_thumbnail_image_url'],
-            viewCount: rawPost['post_view_count'],
-            createdAt: rawPost['post_created_at'],
-            updateAt: rawPost['post_updated_at'],
-            deletedAt: rawPost['post_deleted_at'],
-            likesCount: Number(rawPost['likesCount']),
-            commentsCount: Number(rawPost['commentsCount']),
-          };
-
-          return new PostEntity(postEntity);
-        });
+      .getRawMany();
+    return result.map(rawPost => {
+      const user = new User().fromRaw({
+        user_id: rawPost['user_id'],
+        user_type: rawPost['user_type'],
+        user_name: rawPost['user_name'],
+        user_nickname: rawPost['user_nickname'],
+        user_email: rawPost['user_email'],
+        user_phone: rawPost['user_phone'],
+        user_birth: rawPost['user_birth'],
+        user_password: rawPost['user_password'],
+        user_icon_image_url: rawPost['user_icon_image_url'],
+        user_created_at: rawPost['user_created_at'],
+        user_updated_at: rawPost['user_updated_at'],
+        user_deleted_at: rawPost['user_deleted_at'],
       });
+
+      const postEntity: Partial<PostEntity> = {
+        id: rawPost['post_id'],
+        userId: rawPost['post_user_id'],
+        user: user,
+        category: rawPost['post_category'],
+        title: rawPost['post_title'],
+        contents: rawPost['post_contents'],
+        thumbnailImageUrl: rawPost['post_thumbnail_image_url'],
+        viewCount: rawPost['post_view_count'],
+        createdAt: rawPost['post_created_at'],
+        updateAt: rawPost['post_updated_at'],
+        deletedAt: rawPost['post_deleted_at'],
+        likesCount: Number(rawPost['likesCount']),
+        commentsCount: Number(rawPost['commentsCount']),
+      };
+
+      return new PostEntity(postEntity);
+    });
   }
 
   async findManyPaging(fetcher: PostPagingFetcher): Promise<PagingItems<PostEntity>> {
